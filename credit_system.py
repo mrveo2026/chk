dedddfc1a3d8cbfab389a8a8490ad0788e82623a
mrveo2026 <hackeraunglay2021@gmail.com>
2026-07-02 21:20:55 +0630

    Create credit_system.py

---
+++ credit_system.py
@@ -0,0 +1,16 @@
+# credit_system.py
+from database import *
+
+__all__ = [
+    'get_user_credits',
+    'add_credits', 
+    'deduct_credit',
+    'update_user_stats',
+    'get_user_stats',
+    'create_user',
+    'add_transaction',
+    'get_all_users',
+    'get_user_transactions',
+    'get_total_stats',
+    'init_database'
+]
