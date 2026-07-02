f7fd6a9f7f5af3e5dc622b8fe9791aec7620ef18
mrveo2026 <hackeraunglay2021@gmail.com>
2026-07-02 21:43:55 +0630

    Update credit_system.py

--- credit_system.py
+++ credit_system.py
@@ -5,12 +5,17 @@ __all__ = [
     'get_user_credits',
     'add_credits', 
     'deduct_credit',
+    'transfer_credits',
     'update_user_stats',
     'get_user_stats',
     'create_user',
     'add_transaction',
     'get_all_users',
     'get_user_transactions',
+    'get_transfer_history',
     'get_total_stats',
+    'get_user_by_username',
+    'save_card_result',
+    'get_user_card_results',
     'init_database'
 ]
